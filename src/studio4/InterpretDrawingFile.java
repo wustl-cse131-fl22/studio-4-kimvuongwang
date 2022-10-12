116d0d9b502cc8d6098958ebd63d19b2302ef3fa
carl <carl@10.232.170.66>
2022-10-12 16:41:35 -0500

    Hello

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,38 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+
+		double[] parameters = new double[6];
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
 		
+		for(int i = 0; i < parameters.length; i++) {
+			
+			if(in.hasNextDouble()) {
+				parameters[i] = in.nextDouble();
+			}
+		}
+		
+		System.out.println(shapeType);
+		System.out.println(redComponent);
+		System.out.println(greenComponent);
+		System.out.println(blueComponent);
+		System.out.println(isFilled);
+		
+		for(double i : parameters) {
+			System.out.println(i);
+		}
+		
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+		
+		if(isFilled) {
+			StdDraw.filledRectangle(parameters[0], parameters[1], parameters[2], parameters[3]);
+		}
+		else {
+			StdDraw.rectangle(parameters[0], parameters[1], parameters[2], parameters[3]);
+		}
 	}
 }
